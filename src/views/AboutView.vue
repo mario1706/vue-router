af941078e085c90efbb236c8eea0d24b94178f70
mario1706 <marionurrohman@student.uir.ac.id>
2023-05-20 18:56:13 +0700

    vue router

---
+++ src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Biodata</h1>
+  <table>
+    <tr>
+      <th>Nama</th>
+      <td>Mario Nur Rohman</td>
+    </tr>
+    <tr>
+      <th>Tanggal Lahir</th>
+      <td>17 Juni 2001</td>
+    </tr>
+    <tr>
+      <th>Alamat</th>
+      <td>Jl. Reformasi 2, Gg. harmoni, Bukit Raya, Pekanbaru</td>
+    </tr>
+    <tr>
+      <th>Email</th>
+      <td>marionurrohman@student.uir.a.id</td>
+    </tr>
+    <tr>
+      <th>No. Telepon</th>
+      <td>081364091782</td>
+    </tr>
+  </table>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
